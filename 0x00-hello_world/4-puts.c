9c56fe02dbb935b7ce32cc86d19ef2882c21c572
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-10 15:14:35 +0100

    4. Hello, puts correction finale

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -19,10 +19,9 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
