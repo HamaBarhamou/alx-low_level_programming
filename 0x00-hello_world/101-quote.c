7f8a7640c96a6d394406e0c7de0939847e4949a2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-10 20:15:19 +0100

    tache 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,15 +13,20 @@
 */
 
 #include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
 
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
+
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	system("rm barhamounafissa > sorti 2>&1");
+	exit(1);
 }
 
