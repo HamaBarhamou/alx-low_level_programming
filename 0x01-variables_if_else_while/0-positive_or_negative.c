61f3149a10ebd2a28624b89f5a92b6013fed0cf2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-11 11:24:14 +0100

    0. Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    5-printf.c                                  /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: Barahmou <hamabarhamou@gmail.com>     /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*    Created: 2022-03-10 13:29:43 by Barahmou          \/        \/      \_/ *
+*    Updated: 2022-03-10 13:29:43 by Barahmou                                *
+*                                                                            *
+******************************************************************************
+*/
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n > 0)
+		printf("%d is positive\n",n);
+	else if(n == 0)
+		printf("%d is zero\n",n);
+	else
+		printf("%d is zero\n",n);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+# alx-low_level_programming
+0x00. C - Hello, World </br>
+beautiful the header of my files isn't it? You can make your own with this open source project. very easy to use
+https://github.com/HamaBarhamou/ALX-Header
