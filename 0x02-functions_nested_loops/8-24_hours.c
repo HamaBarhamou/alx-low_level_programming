cbf5df81131d246bb76f98c52a80be9fc857a09f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-16 00:17:12 +0100

    8. I'm federal agent Jack Bauer, and today is the longest day of my life Correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,24 +24,24 @@
 void jack_bauer(void)
 {
 	int a = 0, b = 0, c = 0, d = 0;
+	int n = 9;
 
 	for (a = 0; a <= 2 ; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		if (a == 2)
+			n = 3;
+		for (b = 0; b <= n; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a <= 2 && b <= 3)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 
