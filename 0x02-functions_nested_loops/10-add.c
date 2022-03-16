414374ce432646ce934bbe61f094898ebb0c8fa2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-16 01:45:15 +0100

    0. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -15,43 +15,16 @@
 #include "main.h"
 
 /**
- *times_table - a function that prints the 9 times table, starting with 0
+ *add - a function that adds two integers and returns the result.
  *starting from 00:00 to 23:59.
+ *@a: the number
+ *@b: the number
  *
  *Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately.
  */
-void times_table(void)
+int add(int a, int b)
 {
-	int i = 0, j = 0, k;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			k = i * j;
-			if (k <= 9)
-				_putchar(k + '0');
-			else
-			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			
-			if (j < 9)
-			{
-				_putchar(',');
-				if (k <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-					_putchar(' ');
-			}
-		}
-
-		_putchar('\n');
-	}
+	return (a + b);
 }
 
