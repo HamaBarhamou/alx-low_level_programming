626d9908ea885fdf6dd6c38986c9e9923a129597
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-18 13:30:58 +0100

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nb;
+
+	for (nb = 1; nb <= 100; nb++)
+	{
+		if (nb % 3 == 0)
+		{
+			if (nb % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		else if (nb % 5 == 0)
+		{
+			if (nb % 3 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Buzz ");
+
+		}
+		else
+			printf("%d ", nb);
+	}
+	return (0);
+}
