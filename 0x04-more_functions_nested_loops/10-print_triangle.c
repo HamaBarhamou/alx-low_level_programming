79717824cdaa7bddfbef172c86bb0a041ad91d2d
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-19 02:46:32 +0100

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,21 +9,26 @@
 
 void print_triangle(int size)
 {
-	int i = 0, j = 0, n = size;
-
-
-	if (size  <= 0)
-		_putchar('\n');
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < i; j++)
-		{
-			if(j >= n)
-				_putchar('#');
-			else
-				_putchar(' ');
-		}
 		_putchar('\n');
-		n--;
+	} else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
