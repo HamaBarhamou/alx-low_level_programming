36e35b5757f504ee098ceae83e9cec4323b3a43f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-21 09:43:36 +0100

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * @n: the integer adresse
+ *
+ * Return: 1 or 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
