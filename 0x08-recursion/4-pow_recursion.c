d5698b41297834c2f8a5b201008920c7f725f65b
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-30 12:28:34 +0100

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-puts_recursion.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-30 09:46:22   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-30 09:46:22 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/**
+* _pow_recursion - function
+*
+* @x: the number
+* @y: the number
+* Return: Always 0.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
+
