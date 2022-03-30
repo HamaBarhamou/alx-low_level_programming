6552e92b3fbb2227cd460ace28820fab0b81970f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-30 10:27:20 +0100

    1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-puts_recursion.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-30 09:46:22   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-30 09:46:22 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "main.h"
+
+/**
+* _print_rev_recursion - function
+*
+* @s: the chaine
+* Return: Always 0.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
