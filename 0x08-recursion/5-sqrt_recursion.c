cbcece3b7bba10c95b02aa7d677aeb5ab0ee8f82
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-30 15:55:28 +0100

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-puts_recursion.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-30 09:46:22   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-30 09:46:22 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+#include<stdio.h>
+/**
+ * _sqrt - function
+ * @n: the number
+ * @i: the number
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i >  n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
+}
+
+/**
+* _sqrt_recursion - function
+*
+* @n: the number
+* Return: Always 0.
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(n, 0));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,6 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 #endif /* MAIN_H */
