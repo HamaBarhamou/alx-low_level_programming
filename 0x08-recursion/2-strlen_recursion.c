5d750dd56dfd7a5ec137aaf4e325223c923cbec6
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-30 10:52:51 +0100

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,31 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-puts_recursion.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-30 09:46:22   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-30 09:46:22 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "main.h"
+int taille = 0;
+/**
+* _strlen_recursion - function
+*
+* @s: the chaine
+* Return: Always 0.
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (s[taille] != '\0')
+	{
+		taille++;
+		_strlen_recursion(s);
+	}
+	return (taille);
+}
+
