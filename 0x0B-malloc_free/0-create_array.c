b0ee2d9c1baa4b2245b3c7dbf2eda7855bd47acd
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-04 09:23:53 +0100

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+
+/**
+ * create_array - a function ...
+ * @size: the int
+ * @c: the char
+ *
+ * Return: 1 or 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *tab;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	tab = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+		tab[i] = c;
+
+	return (tab);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+# alx-low_level_programming
+0x00. C - Hello, World </br>
+beautiful the header of my files isn't it? You can make your own with this open source project. very easy to use
+https://github.com/HamaBarhamou/ALX-Header</br></br>
+
