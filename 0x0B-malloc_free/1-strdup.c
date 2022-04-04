044cffa2acad76a3833f8c3130937b4acabb421f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-04 11:32:09 +0100

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+
+/**
+ * _strdup - a function ...
+ * @str: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+char  *_strdup(char *str)
+{
+	char *src;
+	int len = 0, i = 0;
+
+	while (str[len])
+		len++;
+	src = malloc((len + 1) * sizeof(char));
+	while (str[i])
+	{
+		src[i] = str[i];
+		i++;
+	}
+	src[i] = '\0';
+	return (src);
+}
+
