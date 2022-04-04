8fe3252da776425197bd4a38ed6357333a0156d2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-04 22:16:20 +0100

    4. It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * free_grid - a function ...
+ * @grid: the matrice
+ * @height: the number
+ *
+ * Return: 1 or 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
+
