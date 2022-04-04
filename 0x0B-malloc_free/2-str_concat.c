cf888bf018ebbbe5b3e30cf07c9c688ca237032d
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-04 18:36:19 +0100

    2. He who is not courageous enough to take risks will accomplish nothing in life correction 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -63,8 +63,6 @@ char  *str_concat(char *s1, char *s2)
 	char *src;
 	int len1 = 0, i = 0, len2 = 0, j = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
