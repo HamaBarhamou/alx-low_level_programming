4b8820f79e2d3bfa82a3ed1a4d3f1f5cd9652d9c
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-11 14:43:19 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
