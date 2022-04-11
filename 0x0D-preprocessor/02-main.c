2d91056047c346b7e6298690c428c9e2cf2a8353
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-11 13:32:27 +0100

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (__FILE__[i])
+	{
+		_putchar(__FILE__[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
