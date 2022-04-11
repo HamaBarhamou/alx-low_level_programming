e9f70868dad99901e7e9e66f4e136947582d8c14
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-11 21:59:41 +0100

    4. A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - a function ...
+ * @name: the char
+ * @age: the int
+ * @owner: the char
+ *
+ * Return: 1 or 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
+
