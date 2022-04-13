0195a0fb6aa58c97c46bbb5e00d188de4200f1eb
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-13 15:17:20 +0100

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stddef.h>
+
+/**
+ * int_index - a function ...
+ * @array: the tab of integer
+ * @size: the number
+ * @cmp: the function
+ *
+ * Return: 1 or 0
+ */
+
+int int_index(int *array, size_t size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (size <= 0 || array == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
+
