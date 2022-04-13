aa5afd286b14d5b4c3d4f51ac8739bae4d2ca87c
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-13 12:45:06 +0100

    1. If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,31 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stddef.h>
+
+/**
+ * array_iterator - a function ...
+ * @array: the tab of integer
+ * @size: the number
+ * @action: the function
+ *
+ * Return: 1 or 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
+
