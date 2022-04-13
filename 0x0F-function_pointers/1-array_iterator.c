66558e5462b154fc7b6735ebbe75680a151fd811
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-13 13:13:55 +0100

    ajout de condition dans 1-arr... correction 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -25,7 +25,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		(*action)(array[i]);
