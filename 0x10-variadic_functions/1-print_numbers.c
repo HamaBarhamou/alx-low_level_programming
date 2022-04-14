81f89705cfc095bac82bc8f364fa1b10147819ab
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-14 15:20:33 +0100

    1. To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,44 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdio.h>
+
+/**
+ * print_numbers - a function ...
+ * @separator: the chaine
+ * @n: the number
+ * @...: the char
+ *
+ * Return: 1 or 0
+ */
+void print_numbers(char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(ap, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ap, int), separator);
+	printf("%d", va_arg(ap, int));
+	printf("\n");
+	va_end(ap);
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* MAIN_H */
