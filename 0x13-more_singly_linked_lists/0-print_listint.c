49cb8b60a45795d83b6c75dda47548c6a334e68b
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-25 04:20:56 +0100

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,39 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-print_listint.c                            /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - a function ...
+ * @h: the list
+ *
+ * Return: 1 or 0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr;
+	int cpt = 0;
+
+	if (h == NULL)
+		return (0);
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		cpt++;
+	}
+	return (cpt);
+}
+
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include<stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+#endif /* MAIN_H */
