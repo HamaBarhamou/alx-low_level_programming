d187872ceb2105dfd7d9775b8d42de1736c3c220
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-26 16:15:50 +0100

    9. Insert correction 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (ptr == NULL)
+	if (ptr == NULL || idx == 0)
 	{
 		*head = new;
 		return (*head);
