b55ffb41ff8e8f9e98071bf73c5772bf4496b4d6
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-26 15:49:14 +0100

    9. Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,61 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 9-insert_nodeint.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * insert_nodeint_at_index - a function ...
+ * @head: the list
+ * @n: the chaine
+ * @idx: the number
+ *
+ * Return: 1 or 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *ptr = *head, *save;
+	unsigned int cpt = 0;
+
+	new =  malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (ptr == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (cpt != idx - 1 && ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		cpt++;
+	}
+
+	if (ptr->next == NULL)
+		ptr->next = new;
+	else
+	{
+		save = ptr->next;
+		ptr->next = new;
+		new->next = save;
+	}
+	return (new);
+}
+
