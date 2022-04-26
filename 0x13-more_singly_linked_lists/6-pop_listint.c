a3239f2e205bf5ec3c3af0e61bc79cb28cda353a
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-26 13:35:05 +0100

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,36 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 6-pop_listint.c                              /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - a function ...
+ * @head: the list
+ *
+ * Return: 1 or 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp, *t;
+
+	if (head == NULL)
+		return (0);
+	tmp = *head;
+	t = *head;
+	t = t->next;
+	*head = t;
+	free(tmp);
+	return (1);
+}
+
