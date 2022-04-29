9e44bbd958b599726ebba648aeb13d7d80e2b375
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-04-29 10:29:46 +0100

    2. 10 correction 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,37 +3,6 @@
 #include <stdio.h>
 
 /**
- * rev_string - a function that reverse a string
- * @s: chaine of caractere
- *
- * Return: 1 or 0
- */
-
-void rev_string(char *s)
-{
-	int i = 0, taille, k;
-	char c;
-
-
-	while (s[i] != '\0')
-		i++;
-
-	i--;
-	taille = i;
-	k = i / 2;
-	i = 0;
-
-	while (i <= k)
-	{
-		c = s[i];
-		s[i] = s[taille];
-		s[taille] = c;
-		i++;
-		taille--;
-	}
-}
-
-/**
  * get_bit - a function ....
  * @n: the chaine of caractere
  * @index: the number
@@ -42,20 +11,12 @@ void rev_string(char *s)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = ~(~0U >> 1);
-	unsigned long int i = 0, k = 0;
-	char t[32];
+	int bit_val;
 
-	while (mask > 0)
-	{
-		t[k] = (n & mask) ? '1' : '0';
-		mask >>= 1;
-		++i;
-		++k;
-	}
-	t[k] = '\0';
-	/*printf("avant: %s\n",t);*/
-	rev_string(t);
-	/*printf("apres: %s\n",t);*/
-	return (t[index] - '0');
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
