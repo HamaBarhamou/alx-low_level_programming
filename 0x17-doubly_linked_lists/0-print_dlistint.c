cacbebfa0742d51c0aca4ae2aacdef6de3e26be5
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 14:02:08 +0100

    0. Print list encore une tentative 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,6 +12,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nb = 0;
 
+	if (h == NULL)
+		return (0);
+
+	if (h->prev != NULL)
+		while (h->prev != NULL)
+			h = h->prev;
+
 	while (h != NULL)
 	{
 		printf("%d \n", h->n);
