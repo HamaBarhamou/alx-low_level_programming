cb24b719e034cf21c701964a211e05e1177831c3
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 13:24:15 +0100

    correction 2. Add node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,9 +13,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t nb = 0;
 	const dlistint_t *ptr;
 
-	/*Recherche de la tete de liste*/
-	while (h->prev != NULL)
-		h = h->prev;
 	ptr = h;
 	while (ptr != NULL)
 	{
