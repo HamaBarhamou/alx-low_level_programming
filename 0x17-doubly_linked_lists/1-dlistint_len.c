894a2768f313ef5d125540ab284d330e152c3f31
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 14:20:07 +0100

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,9 +12,12 @@ size_t dlistint_len(const dlistint_t *h)
 	size_t nb = 0;
 	const dlistint_t *ptr;
 
+	if (h == 0)
+		return (nb)
 	/*Recherche de la tete de liste*/
-	while (h->prev != NULL)
-		h = h->prev;
+	if (h->prev != NULL)
+		while (h->prev != NULL)
+			h = h->prev;
 	ptr = h;
 	while (ptr != NULL)
 	{
