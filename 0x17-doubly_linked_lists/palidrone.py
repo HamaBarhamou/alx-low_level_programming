89b0bc3211ab3c07c24530dfe0ae04c189a2de1a
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 20:44:14 +0100

    10. Palindromes

---
+++ 0x17-doubly_linked_lists/palidrone.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def check_palindrome(v):
+    reverse = v[::-1]
+
+    if (v == reverse):
+        return True
+    return False
+
+pali = 0
+for num in range(999*999 + 1):
+    if check_palindrome(str(num)):
+        pali = num
+
+print(pali)
