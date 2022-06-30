e063c9976ceb873640b6599de046b1c02d344bcc
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-30 21:59:58 +0100

    0. >>> ht = {} correction 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
-	hash_node_t *tab;
 	unsigned long int i;
 
 	hash = malloc(sizeof(hash_table_t));
@@ -18,12 +17,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash->size = size;
-	tab = malloc(sizeof(hash_node_t *) * size);
-	hash->array = &tab;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash->array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		hash->array = NULL;
+		hash->array[i] = NULL;
 	return (hash);
 }
