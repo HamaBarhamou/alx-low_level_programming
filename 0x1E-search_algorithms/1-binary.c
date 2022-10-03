d5daa2eb3c1d7bc8021b21fa7154d1450ad8df5f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-03 22:44:19 +0100

    1. Binary search tentative 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* binary_search - a function
+* @array: the array
+* @size: size
+* @value: a number
+*
+* Return: 1 or 0
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t L = 0, R = size - 1;
+	int m;
+
+	while (L != R)
+	{
+		/*m = ceil((L + R) / 2);*/
+		m = (L + R) / 2;
+		if (array[m] > value)
+			R = m - 1;
+		else
+			L = m;
+		/*printf("\n L = %ld  R = %ld",L, R);*/
+	}
+	if (array[L] == value)
+		return (L);
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
