cc0ee706ae7ea11a1889def96cc04309f0ae9c02
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-03 12:42:02 +0100

    0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,38 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#include<stdlib.h>
+
+/**
+* linear_search - a function
+* @array: the array
+* @size: size
+* @value: a number
+*
+* Return: 1 or 0
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
